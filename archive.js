5e1469c195f848c020606ef47530256d6dfccb5f
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-07-18 10:59:11 +0100

    update

---
+++ archive.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  var dt = new Date();
+  var year = dt.getYear() + 1900;
+  var month = dt.getMonth();
+  var cont = document.getElementById('leader_cont');
+  
+  for (var i = 2017; i <= year; i++) {
+    for (var j = 1; j <= (i == year ? month : 12); j++) {
+      var k = "archives/" + i + "_" + (j < 10 ? "0" + j : j) + ".png";
+      cont.innerHTML += "<img src='" + k + "' />";
+    }
+  }
+}
