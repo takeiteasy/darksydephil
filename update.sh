dba724d28c88b2b71dd5c4ef63bbf91d95dab74e
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-08-05 05:30:00 +0100

    linux requires bash for some reason

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ perl get_logs.pl $1 $2
 perl get_paymetonnes.pl
 perl generate.pl
 
-sh update_monthly.sh $1 $2
+bash update_monthly.sh $1 $2
 
 sh update_www.sh
--- update_monthly.sh
+++ update_monthly.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 MONTHS=(January February March April May June July August September October November December)
 
