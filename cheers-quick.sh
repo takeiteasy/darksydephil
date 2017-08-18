30c137aacfd9b537a7b72c0bddb53dc7342db28d
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-08-18 13:52:31 +0100

    durr

--- cheers-quick.sh
+++ cheers-quick.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sS "https://overrustlelogs.net/Darksydephil%20chatlog/August%202017/$1.txt" | grep -P '[?<=\s]cheer(\d+)(?!\S)' | grep "$2"
+curl -sS "https://overrustlelogs.net/Darksydephil%20chatlog/August%202017/2017-08-$1.txt" | grep -P '[?<=\s]cheer(\d+)(?!\S)' | grep "$2"
