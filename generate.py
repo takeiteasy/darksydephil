d2c1cbc8c08b05cb8ce739c0e40fd10e8d715598
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-09-01 20:47:53 +0100

    parsing for formatted 2016 logs

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-import os, time
+import os, time, re
 
 def is_leap_year(y):
     return y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)
@@ -11,6 +11,8 @@ month_days  = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 c_year, c_month, c_month_n, c_day = time.strftime("%Y/%B/%m/%d").split("/")
+sub_re = re.compile(r'^\[(\d){2}:(\d){2}\] <twitchnotify> \S+ (.*)$')
+cheer_re = re.compile(r'[?<=\s](?:kappa|kreygasm|swiftrage|cheer)(\d+)(?!\S)')
 users = {}
 data = {}
 for year in range(2016, int(c_year) + 1):
@@ -21,10 +23,10 @@ for year in range(2016, int(c_year) + 1):
             for day in range(1, (29 if is_leap_year(year) else 28 if month == 1 else month_days[month]) + 1):
                 path = "logs/{}/{}/{}-{}-{}.txt".format(year, month_names[month], year, prepend_zero(month + 1), prepend_zero(day))
                 if os.path.exists(path):
-                    data[year][month_names[month]][day] = { 'permabans': [], 'bans': [], 'bits': 0 }
+                    data[year][month_names[month]][day] = { 'permabans': [], 'bans': [], 'bits': 0, 'subs': 0 }
                     with open(path, 'r') as fh:
-                        for line in fh:
-                            if year > 2016:
+                        if year > 2016:
+                            for line in fh:
                                 parts = line.split(' #darksydephil :', 1)
                                 info  = dict(x.split('=') for x in parts[0].split(';'))
                                 msg   = parts[1][:-1] if len(parts) == 2 else ""
@@ -38,8 +40,13 @@ for year in range(2016, int(c_year) + 1):
                                         data[year][month_names[month]][day]['bits'] += int(info['bits'])
                                 else:
                                     data[year][month_names[month]][day]['permabans' if '@ban-duration' in info else 'bans'].append(info['target-user-id'])
-                            else:
-                                pass
+                        else:
+                            for line in fh:
+                                m = sub_re.match(line)
+                                if m:
+                                    data[year][month_names[month]][day]['subs'] += 2.5
+                                else:
+                                    data[year][month_names[month]][day]['bits'] += sum([int(x) for x in cheer_re.findall(line)])
 
 import code
 code.interact(local=locals())
