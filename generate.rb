3bc9c6675e0761f418467fb6ec18490eed7437c0
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-11-06 19:16:32 +0000

    beautify

--- generate.rb
+++ generate.rb
@@ -1,93 +1,95 @@
 #!/usr/bin/env ruby
-require "json"
-require "twitter"
-require "net/http"
+require 'json'
+require 'twitter'
+require 'net/http'
 
-month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month_names = %w[January February March April May June July August September October November December]
 month_days  = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-sub_money   = { "Prime" => 2.5,
-                "1000"  => 2.5,
-                "2000"  => 6.66,
-                "3000"  => 17.5 }
+sub_money   = { 'Prime' => 2.5,
+                '1000'  => 2.5,
+                '2000'  => 6.66,
+                '3000'  => 17.5 }
 
-def is_leap y
+def is_leap(y)
   y % 4 == 0 && (y % 100 != 0 || y % 400 == 0)
 end
 
-def date_suffix n
-  if (11..13).include? n.to_i.abs % 100
-    "th"
+def date_suffix(n)
+  if (11..13).cover? n.to_i.abs % 100
+    'th'
   else
     case n.to_i.abs % 10
-    when 1 then "st"
-    when 2 then "nd"
-    when 3 then "rd"
-    else        "th"
+    when 1 then 'st'
+    when 2 then 'nd'
+    when 3 then 'rd'
+    else        'th'
     end
   end
 end
 
 now = Time.now
-#last_month = month_names[(now.month == 12 ? 1 : now.month) - 1]
-last_month = "August" # TODO: Change back
-data, users, paypigs, last_month_paypigs = {}, {}, {}, {}
+# last_month = month_names[(now.month == 12 ? 1 : now.month) - 1]
+last_month = 'August' # TODO: Change back
+data = {}
+users = {}
+paypigs = {}
+last_month_paypigs = {}
 2016.upto now.year do |year|
   data[year] = {}
   0.upto 11 do |month_n|
     month = month_names[month_n]
     data[year][month] = {}
     1.upto (month_n == 1 ? (is_leap(year) ? 29 : 28) : month_days[month_n]) do |day|
-      fh_p = "./logs/#{year}/#{month}/#{year}-#{(month_n + 1).to_s.rjust(2, "0")}-#{day.to_s.rjust(2, "0")}.txt"
-      if File.exists? fh_p
-        data[year][month][day] = { "permabans" => 0,
-                                   "bans"      => 0,
-                                   "bits"      => 0,
-                                   "subs"      => 0 }
-        File.open fh_p, 'r' do |fh|
-          while line = fh.gets
-            unless year == 2016
-              next unless line[0] == '@'
-
-              /^@(?<params>[a-zA-Z-]+=\S*)\s?:((?<user>\S+!\S+@\S+)\.)?tmi\.twitch\.tv (?<type>\S+) #\S+( :(?<msg>.*))?$/ =~ line
-              unless $~.nil?
-                params = params.split(';').reject { |a| a[-1] == '=' }.map { |b| b.split '=' }.to_h
-                user   = user.split('!')[0] unless user.nil?
-
-                if params.has_key? "user-id"
-                  users[params["user-id"]] = [] unless users.has_key? "user-id"
-                  if params.has_key?("display-name") && !users[params["user-id"]].include?(params["display-name"])
-                    users[params["user-id"]].push params["display-name"]
-                  else
-                    unless user.nil?
-                      name = user.split('!')[0]
-                      users[params["user-id"]].push name unless users[params["user-id"]].include? name
-                    end
-                  end
-
-                  if params.has_key? "bits"
-                    bits = params["bits"].to_i
-                    data[year][month][day]["bits"] += bits
-                    paypigs[params["user-id"]]  = 0 unless paypigs.has_key? params["user-id"]
-                    paypigs[params["user-id"]] += bits
-                    if year == now.year && month == last_month
-                      last_month_paypigs[params["user-id"]]  = 0 unless last_month_paypigs.has_key? params["user-id"]
-                      last_month_paypigs[params["user-id"]] += bits
-                    end
-                  elsif params.has_key? "msg-param-sub-plan"
-                    data[year][month][day]["subs"] += sub_money[params["msg-param-sub-plan"]]
-                  elsif params.has_key?("subscriber") && params["subscriber"] == "1" && params.has_key?("system-msg") && params["system-msg"] =~ /.*\\ssubscribed\\s/
-                    data[year][month][day]["subs"] += 2.5;
-                  end
-                else
-                  data[year][month][day][(params.has_key?("ban-duration") ? "bans" : "permabans")] += 1
-                end
-              end
+      fh_p = "./logs/#{year}/#{month}/#{year}-#{(month_n + 1).to_s.rjust(2, '0')}-#{day.to_s.rjust(2, '0')}.txt"
+      next unless File.exist? fh_p
+      data[year][month][day] = { 'permabans' => 0,
+                                 'bans'      => 0,
+                                 'bits'      => 0,
+                                 'subs'      => 0 }
+      File.open fh_p, 'r' do |fh|
+        while line = fh.gets
+          if year == 2016
+            if line =~ /^\[(\d){2}:(\d){2}\] <twitchnotify> \S+ (.*)$/
+              data[year][month][day]['subs'] += 2.5
             else
-              if line =~ /^\[(\d){2}:(\d){2}\] <twitchnotify> \S+ (.*)$/
-                data[year][month][day]["subs"] += 2.5
+              m = line.scan(/[?<=\s](?:kappa|kreygasm|swiftrage|cheer)(\d+)(?!\S)/)
+              data[year][month][day]['bits'] += m.flatten(1).map(&:to_i).inject(0, :+) unless m.empty?
+            end
+          else
+            next unless line[0] == '@'
+
+            /^@(?<params>[a-zA-Z-]+=\S*)\s?:((?<user>\S+!\S+@\S+)\.)?tmi\.twitch\.tv (?<type>\S+) #\S+( :(?<msg>.*))?$/ =~ line
+            unless $LAST_MATCH_INFO.nil?
+              params = params.split(';').reject { |a| a[-1] == '=' }.map { |b| b.split '=' }.to_h
+              user   = user.split('!')[0] unless user.nil?
+
+              if params.key? 'user-id'
+                users[params['user-id']] = [] unless users.key? 'user-id'
+                if params.key?('display-name') && !users[params['user-id']].include?(params['display-name'])
+                  users[params['user-id']].push params['display-name']
+                else
+                  unless user.nil?
+                    name = user.split('!')[0]
+                    users[params['user-id']].push name unless users[params['user-id']].include? name
+                  end
+                end
+
+                if params.key? 'bits'
+                  bits = params['bits'].to_i
+                  data[year][month][day]['bits'] += bits
+                  paypigs[params['user-id']]  = 0 unless paypigs.key? params['user-id']
+                  paypigs[params['user-id']] += bits
+                  if year == now.year && month == last_month
+                    last_month_paypigs[params['user-id']]  = 0 unless last_month_paypigs.key? params['user-id']
+                    last_month_paypigs[params['user-id']] += bits
+                  end
+                elsif params.key? 'msg-param-sub-plan'
+                  data[year][month][day]['subs'] += sub_money[params['msg-param-sub-plan']]
+                elsif params.key?('subscriber') && params['subscriber'] == '1' && params.key?('system-msg') && params['system-msg'] =~ /.*\\ssubscribed\\s/
+                  data[year][month][day]['subs'] += 2.5
+                end
               else
-                m = line.scan(/[?<=\s](?:kappa|kreygasm|swiftrage|cheer)(\d+)(?!\S)/)
-                data[year][month][day]["bits"] += m.flatten(1).map(&:to_i).inject(0, :+) unless m.empty?
+                data[year][month][day][(params.key?('ban-duration') ? 'bans' : 'permabans')] += 1
               end
             end
           end
@@ -98,12 +100,12 @@ data, users, paypigs, last_month_paypigs = {}, {}, {}, {}
   end
 end
 
-paypigs = paypigs.sort_by { |k, v| v }.reverse[0..30].map { |v| [users[v[0]][0], v[1]]}.to_h.to_json
-last_month_paypigs = last_month_paypigs.sort_by { |k, v| v }.reverse[0..30].map { |v| [users[v[0]][0], v[1]]}.to_h.to_json
+paypigs = paypigs.sort_by { |_k, v| v }.reverse[0..30].map { |v| [users[v[0]][0], v[1]] }.to_h.to_json
+last_month_paypigs = last_month_paypigs.sort_by { |_k, v| v }.reverse[0..30].map { |v| [users[v[0]][0], v[1]] }.to_h.to_json
 
-pt = /var dataJson = '(.*)'/.match(Net::HTTP.get(URI("https://graphtreon.com/creator/darksydephil")))[1]
-yt = /"Date,Average Views\\n" \+ (.*) {/.match(Net::HTTP.get(URI("https://socialblade.com/youtube/user/dspgaming/monthly")))[1].scan(/(\d+-\d+-\d+,\d+)/).map { |x| x[0].split ',' }.to_h.to_json
-lp = Net::HTTP.get(URI("https://www.patreon.com/darksydephil")).scan(/pledge_sum": (\d+)/)[0][0]
+pt = /var dataJson = '(.*)'/.match(Net::HTTP.get(URI('https://graphtreon.com/creator/darksydephil')))[1]
+yt = /"Date,Average Views\\n" \+ (.*) {/.match(Net::HTTP.get(URI('https://socialblade.com/youtube/user/dspgaming/monthly')))[1].scan(/(\d+-\d+-\d+,\d+)/).map { |x| x[0].split ',' }.to_h.to_json
+lp = Net::HTTP.get(URI('https://www.patreon.com/darksydephil')).scan(/pledge_sum": (\d+)/)[0][0]
 
 puts "var data = '#{data.to_json}';\nvar dataJson = '#{pt}'\nvar yt_data = '#{yt}';\nvar paypigs = '#{paypigs}';\nvar last_paypigs = '#{last_month_paypigs}';\nvar last_patreon = #{lp.to_i / 100};\nvar last_update = #{now.to_i};"
 
@@ -122,7 +124,7 @@ else
 end
 month = month_names[month - 1]
 
-a, b, c, d = File.open("twitter.conf").readlines.map(&:rstrip)
+a, b, c, d = File.open('twitter.conf').readlines.map(&:rstrip)
 client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = a
   config.consumer_secret     = b
--- twitch.rb
+++ twitch.rb
@@ -1,179 +1,178 @@
 #!/usr/bin/env ruby
-require "socket"
-require "timeout"
-require "FileUtils"
-require "gmail"
-require "thread"
+require 'socket'
+require 'timeout'
+require 'FileUtils'
+require 'gmail'
+require 'thread'
 
 # Extend IO to readlines without blocking
 class IO
-	def gets_nonblock
-		@rlnb_buffer ||= ""
-		ch = nil
-		while ch = self.read_nonblock(1)
+  def gets_nonblock
+    @rlnb_buffer ||= ''
+    ch = nil
+    while ch = read_nonblock(1)
       @rlnb_buffer += ch
-			if @rlnb_buffer[-1].ord == 10 && @rlnb_buffer[-2].ord == 13
-				res  = @rlnb_buffer[0..-3]
-				@rlnb_buffer = ""
-				return res
-			end
-		end
-	end
+      next unless @rlnb_buffer[-1].ord == 10 && @rlnb_buffer[-2].ord == 13
+      res = @rlnb_buffer[0..-3]
+      @rlnb_buffer = ''
+      return res
+    end
+  end
 end
 
 # Class to emit events
 module Emitter
-	def callbacks
-		@callbacks ||= Hash.new { |h, k| h[k] = [] }
-	end
+  def callbacks
+    @callbacks ||= Hash.new { |h, k| h[k] = [] }
+  end
 
-	def on type, &block
-		callbacks[type] << block
-		self
-	end
+  def on(type, &block)
+    callbacks[type] << block
+    self
+  end
 
-	def emit type, *args
-		callbacks[type].each do |block|
-			block.call(*args)
-		end
-	end
+  def emit(type, *args)
+    callbacks[type].each do |block|
+      block.call(*args)
+    end
+  end
 end
 
 # Class to handle IRC stream and emit events
 class Stream
-	include Emitter
-	attr_accessor :io, :buf
+  include Emitter
+  attr_accessor :io, :buf
 
-	def initialize serv, port
-		@buf = []
-		Timeout.timeout(5) do
-		  @io = TCPSocket.new serv, port
-		end
-	rescue SocketError => e
-		puts "ERROR! Failed to connect to #{serv}! #{e.message}"
-		self.disconnect
-	rescue Timeout::Error
-		puts "ERROR! Connection to #{serv} timed out!"
-		self.disconnect
-	end
+  def initialize(serv, port)
+    @buf = []
+    Timeout.timeout(5) do
+      @io = TCPSocket.new serv, port
+    end
+  rescue SocketError => e
+    puts "ERROR! Failed to connect to #{serv}! #{e.message}"
+    disconnect
+  rescue Timeout::Error
+    puts "ERROR! Connection to #{serv} timed out!"
+    disconnect
+  end
 
-	def disconnect
-		@io.puts 'QUIT'
-		@io.close
-	rescue Errno::EPIPE
-	end
+  def disconnect
+    @io.puts 'QUIT'
+    @io.close
+  rescue Errno::EPIPE
+  end
 
-	def << data
-		@buf << data
-	end
+  def <<(data)
+    @buf << data
+  end
 
-	def write
-		@buf.each do |x|
-			@io.puts x
-			emit :WROTE, x
-		end
-		@buf = []
-	rescue EOFError, Errno::ECONNRESET
-		emit :CLOSED
-	end
+  def write
+    @buf.each do |x|
+      @io.puts x
+      emit :WROTE, x
+    end
+    @buf = []
+  rescue EOFError, Errno::ECONNRESET
+    emit :CLOSED
+  end
 
-	def read
-		read = @io.gets_nonblock
-		emit :READ, read
-	rescue IO::WaitReadable
-		emit :WAITING
-	rescue EOFError, Errno::ECONNRESET
-		emit :CLOSED
-	end
+  def read
+    read = @io.gets_nonblock
+    emit :READ, read
+  rescue IO::WaitReadable
+    emit :WAITING
+  rescue EOFError, Errno::ECONNRESET
+    emit :CLOSED
+  end
 end
 
 # Class to handle IRC stream
 class Bot
-	attr_reader :stream
+  attr_reader :stream
 
-	def initialize stream
-		@stream = stream
-		@stream.on :CLOSED do
+  def initialize(stream)
+    @stream = stream
+    @stream.on :CLOSED do
       stop
       @steam.disconnect
     end
-	end
+  end
 
-	def start
-		@running = true
-		tick while @running
-	end
+  def start
+    @running = true
+    tick while @running
+  end
 
-	def stop
-		@running = false
-	end
+  def stop
+    @running = false
+  end
 
-	def tick
-		@stream.read
-		@stream.write
-		sleep 0.001
-	end
+  def tick
+    @stream.read
+    @stream.write
+    sleep 0.001
+  end
 end
 
 bot  = nil
-user = "justinfan" + (0...10).map { ('0'..'9').to_a[rand(10)] }.join
-chan = "darksydephil"
-out  = "./logs/%Y/%B/%Y-%m-%d.txt"
+user = 'justinfan' + (0...10).map { ('0'..'9').to_a[rand(10)] }.join
+chan = 'darksydephil'
+out  = './logs/%Y/%B/%Y-%m-%d.txt'
 fh   = nil
-fh_p = ""
-tz   = "-07:00"
+fh_p = ''
+tz   = '-07:00'
 login, password = File.open('gmail.conf').readlines.map(&:rstrip)
 
 begin
   Gmail.connect login, password do |gmail|
-    gmail.inbox.find(:unread, :from => "no-reply@twitch.tv").each do |email|
-      if email.message.subject == "darksydephil just went live on Twitch"
-        email.read!
+    gmail.inbox.find(:unread, from: 'no-reply@twitch.tv').each do |email|
+      next unless email.message.subject == 'darksydephil just went live on Twitch'
+      email.read!
 
-        sock = Stream.new "irc.chat.twitch.tv", 6667
-        bot  = Bot.new sock
+      sock = Stream.new 'irc.chat.twitch.tv', 6667
+      bot  = Bot.new sock
 
-        sock << "USER #{user} 0 0 :#{user}"
-        sock << "NICK #{user}"
-        sock << "CAP REQ :twitch.tv/membership"
-        sock << "CAP REQ :twitch.tv/tags"
-        sock << "CAP REQ :twitch.tv/commands"
-        sock << "JOIN ##{chan}"
+      sock << "USER #{user} 0 0 :#{user}"
+      sock << "NICK #{user}"
+      sock << 'CAP REQ :twitch.tv/membership'
+      sock << 'CAP REQ :twitch.tv/tags'
+      sock << 'CAP REQ :twitch.tv/commands'
+      sock << "JOIN ##{chan}"
 
-				last_msg, timeout = Time.now, 900
-				Thread.new do
-					loop do
-						bot.stop if Time.now - last_msg > timeout
-						sleep 0.001
-					end
-				end
-
-        sock.on :READ do |data|
-          if data.start_with? "PING"
-            data[1] = 'O'
-            sock << data
-          else
-            time = Time.now.getlocal(tz)
-						last_msg = Time.now
-            path = time.strftime out
-            unless path == fh_p
-              fh_p = path
-              fh.close unless fh.nil?
-              fh_p_dn = File.dirname fh_p
-              FileUtils.mkdir_p fh_p_dn unless File.directory? fh_p_dn
-              fh = File.open fh_p, "a+"
-            end
-            fh.write data + "\n"
-            bot.stop if data.start_with? "@msg-id=host_on"
-          end
+      last_msg = Time.now
+      timeout = 900
+      Thread.new do
+        loop do
+          bot.stop if Time.now - last_msg > timeout
+          sleep 0.001
         end
-
-        bot.start
       end
+
+      sock.on :READ do |data|
+        if data.start_with? 'PING'
+          data[1] = 'O'
+          sock << data
+        else
+          time = Time.now.getlocal(tz)
+          last_msg = Time.now
+          path = time.strftime out
+          unless path == fh_p
+            fh_p = path
+            fh.close unless fh.nil?
+            fh_p_dn = File.dirname fh_p
+            FileUtils.mkdir_p fh_p_dn unless File.directory? fh_p_dn
+            fh = File.open fh_p, 'a+'
+          end
+          fh.write data + "\n"
+          bot.stop if data.start_with? '@msg-id=host_on'
+        end
+      end
+
+      bot.start
     end
   end
 rescue SystemExit, Interrupt
-	bot.stop unless bot.nil?
+  bot.stop unless bot.nil?
 rescue EOFError, Exception
 ensure
   fh.close unless fh.nil?
