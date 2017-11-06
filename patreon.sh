2cc81d7d0ac436dc2aaf23af77dd42353500bd2a
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-11-06 20:33:49 +0000

    temporary paths added

--- patreon.sh
+++ patreon.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-echo $(echo '{ "'$(date +%s)'": { "patrons":' $(curl -Ss https://www.patreon.com/darksydephil | grep -E "pledge_sum|patron_count" | head -n 2 | sed -e 's/.*: //' | tr -d '\n' | cut -d ',' -f1-2 | sed -e 's/,/, "earnings": /') "} }") $(tee < data.json) | jq -M -c -S -s add | sponge $2
+echo $(echo '{ "'$(date +%s)'": { "patrons":' $(curl -Ss https://www.patreon.com/$1 | grep -E "pledge_sum|patron_count" | head -n 2 | sed -e 's/.*: //' | tr -d '\n' | cut -d ',' -f1-2 | sed -e 's/,/, "earnings": /') "} }") $(tee < data.json) | /usr/bin/local/jq -M -c -S -s add | /usr/bin/local/sponge $2
